fcfc4fd943e5fd0d2e187d86af6b010b9768b700
bek <bekchanboyusmanv19@gmail.com>
2023-06-11 13:14:25 +0200

    done

---
+++ m_03/ex00/ClapTrap.cpp
@@ -0,0 +1,82 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "ClapTrap " << name << " is born."<< std::endl;
+	this->_name = name;
+	this->_hitpoints = 10;
+	this->_energypoints = 10;
+	this->_attackdamage = 0;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor of ClapTrap called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &c)
+{
+	*this = c;
+}
+
+ClapTrap & ClapTrap::operator = (const ClapTrap &c)
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	this->_name = c._name;
+	this->_hitpoints = c._hitpoints;
+	this->_energypoints = c._energypoints;
+	this->_attackdamage = c._attackdamage;
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (_hitpoints <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " is already dead." << std::endl;
+		return ;
+	}
+	if (_energypoints <= 0)
+	{
+		std::cout << "Claptrap " << _name << " is out of energy" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " attacks " << target << " causing " << _attackdamage << " of damages" << std::endl;
+	_energypoints -= 1;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitpoints <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " is already dead." << std::endl;
+		return ;
+	}
+
+	if (_hitpoints <= amount)
+	{
+		std::cout << "ClapTrap " << _name << " is dead." << std::endl;
+		_hitpoints = 0;
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " takes " << amount << "damage!" << std::endl;
+	_hitpoints -= amount;
+}
+
+void ClapTrap::beReparied(unsigned int amount)
+{
+	if(_hitpoints <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " is already dead." << std::endl;
+		return ;
+	}
+	if (_energypoints <= 0)
+	{
+		
+	}
+}
---
+++ m_03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_CLASS
+#define CLAPTRAP_CLASS
+
+#include <iostream>
+#include <limits.h>
+
+class ClapTrap
+{
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &c);
+		ClapTrap & operator = (const ClapTrap &c);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beReparied(unsigned int amount);
+	private:
+		std::string _name;
+		unsigned int _hitpoints;
+		unsigned int _energypoints;
+		unsigned int _attackdamage;
+};
+
+#endif
