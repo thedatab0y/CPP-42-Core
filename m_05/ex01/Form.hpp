995e14312806f11120083ee15c8cb95de9ee72a5
bek <bekchanboyusmanv19@gmail.com>
2023-10-13 22:50:12 +0200

    added 2

--- m_05/ex01/Form.hpp
+++ m_05/ex01/Form.hpp
@@ -5,6 +5,40 @@
 
 class Bureaucrat;
 
+class Form
+{
+private:
+	const std::string _name;
+	bool _signed;
+	const int _gradeToSign;
+	const int _gradeToExecute;
 
+	Form();
+
+public:
+	Form(const std::string &name, int gradeToSign );
+	Form(const std::string &name, int gradeToSign, int gradeToExecute);
+	Form(const Form & src);
+	~Form();
+
+	Form &operator=(const Form &rhs);
+	std::string getName() const;
+	bool getSigned() const;
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
+
+	void beSigned(const Bureaucrat &bureaucrat);
+
+	class GradeTooHighException : public std::exception{
+		public:
+			virtual const char* what() const throw() {return "Grade too high";}
+	};
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual const char* what() const throw() {return "Grade too low";}
+	};
+};
+
+std::ostream&   operator<<( std::ostream& o, const Form& rhs );
 
 #endif
---
+++ m_05/ex01/main.cpp
@@ -0,0 +1,19 @@
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main( void )
+{
+
+    try {
+        Bureaucrat bureaucrat("ash",11);
+        Form form("formName", 10);
+
+        bureaucrat.signForm(form);
+
+        std::cout << form << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
