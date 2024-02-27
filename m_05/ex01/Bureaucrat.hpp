c46997343eebf10ea8f7f7188bd2f2e69ff2e814
Bekchanboy Usmanov <bekchanboyusmanv19@gmail.com>
2024-02-27 16:42:16 +0100

    01 incomplete

--- m_05/ex01/Bureaucrat.hpp
+++ m_05/ex01/Bureaucrat.hpp
@@ -4,45 +4,45 @@
 #include <iostream>
 #include "Form.hpp"
 
-class Form;
+class Form; // Forward declaration of the Form class
 
 class Bureaucrat
 {
-	private:
-		const std::string _name;
-		int 	_grade;
+    private:
+        const std::string _name; // Private member variable to store the name of the bureaucrat
+        int _grade; // Private member variable to store the grade of the bureaucrat
 
-		Bureaucrat();
-	
-	public:
-		Bureaucrat(const std::string& name, int grade);
-		Bureaucrat(const Bureaucrat& src);
-		~Bureaucrat();
+        Bureaucrat(); // Private default constructor
 
-		Bureaucrat& operator=(const Bureaucrat& rhs);
-		std::string getName() const;
-		int getGrade() const;
+    public:
+        Bureaucrat(const std::string& name, int grade); // Public constructor with name and grade parameters
+        Bureaucrat(const Bureaucrat& src); // Copy constructor
+        ~Bureaucrat(); // Destructor
 
-		void incrementGrade();
-		void decrementGrade();
+        Bureaucrat& operator=(const Bureaucrat& rhs); // Assignment operator overload
+        std::string getName() const; // Member function to get the name of the bureaucrat
+        int getGrade() const; // Member function to get the grade of the bureaucrat
 
-		void signForm(Form& form);
+        void incrementGrade(); // Member function to increment the grade of the bureaucrat
+        void decrementGrade(); // Member function to decrement the grade of the bureaucrat
 
-		// Exception Classes
-		class GradeTooHighException : public std::exception {
-			public:
-				virtual const char* what() const throw() {
-					return "Grade too high";
-				}
-		};
-		class GradeTooLowException : public std::exception {
-			public:
-				virtual const char* what() const throw() {
-					return "Grade too low";
-				}
-		};
+        void signForm(Form& form); // Member function to sign a form
+
+        // Exception Classes
+        class GradeTooHighException : public std::exception { // Custom exception class for high grade
+            public:
+                virtual const char* what() const throw() { // Override what() function to provide error message
+                    return "Grade too high";
+                }
+        };
+        class GradeTooLowException : public std::exception { // Custom exception class for low grade
+            public:
+                virtual const char* what() const throw() { // Override what() function to provide error message
+                    return "Grade too low";
+                }
+        };
 };
 
-std::ostream& operator<<(std::ostream& o, const Bureaucrat& rhs);
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& rhs); // Overloaded operator for output stream
 
 #endif
